f253bd632e1f177af042f4e25c22169d4d6e73db
Kathleen Bongco <kathleen.bongco@gmail.com>
2020-04-21 21:44:10 -0400

    working thru leetcode

---
+++ DSNA/workingthruleetcode/removeele.py
@@ -0,0 +1,8 @@
+#Working Through Leetcode
+#https://leetcode.com/problems/remove-element/
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while nums.count(val) > 0:
+            nums.remove(val)
+        return len(nums)
